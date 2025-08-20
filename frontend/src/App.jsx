802a6df6b7e4bce2a83b0c72aaedf1188ed9ad54
Harsh2004R <harshsharmaktm03@gmail.com>
2025-08-20 19:28:09 +0530

    Hero component is now responcive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AllRoutes from "./Routes/AllRoutes";
 import Navbar from "./AppComponents/Navbar.jsx";
-
+import "./Styles/font.css"
 function App() {
   return (
     <>
--- frontend/src/AppComponents/Hero.jsx
+++ frontend/src/AppComponents/Hero.jsx
@@ -1,25 +1,76 @@
 import { useRef, useEffect } from "react";
-import { Box, Center } from "@chakra-ui/react";
-
+import { Box, Center, Text, Heading } from "@chakra-ui/react";
+import Typewriter from "typewriter-effect";
 export default function Hero() {
-  const backgroundVideo =
-    "https://res.cloudinary.com/djbe55v48/video/upload/v1755673751/memoirra_assets/HomeV1_otg17v.mp4";
+  const backgroundVideo ="https://res.cloudinary.com/djbe55v48/video/upload/v1755673751/memoirra_assets/HomeV1_otg17v.mp4";
   const videoRef = useRef();
   useEffect(() => {
     const video = videoRef.current;
     video.play();
   }, []);
   return (
-    <Center width="100%" height="100vh">
+    <Center
+      width="100%"
+      flexDirection={{ base: "column", md: "row", lg: "row" }}
+      // border={"2px solid blue"}
+      minH={{ base: "50vh", md: "80vh", lg: "60vh" }}
+    >
       <Box
-       width="50%"
-        height="50vh"
+        as="section"
+        w={{ base: "100%", md: "40%" ,lg:"40%"}}
+        minH={{base:"30vh",md:"80vh",lg:"60vh"}}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+        color="white"
+        textAlign="center"
+        px={2}
+      >
+        <Heading
+          fontFamily={"heading-otf"}
+          fontSize={{ base: "3xl", md: "5xl" }}
+          mb={4}
+          color="#fff"
+        >
+          Welcome to{" "}
+          <Text as="span" color="#ef8126">
+            Memoirra
+          </Text>
+        </Heading>
+
+        <Text
+          as="div"
+          fontSize={{ base: "xl", md: "2xl" }}
+          textShadow="0 0 25px #0062ffff"
+          color={"#ffffffff"}
+          fontWeight="medium"
+          fontFamily={"text"}
+        >
+          <Typewriter
+            options={{
+              strings: [
+                "Your Digital Vault",
+                "Store your gmail | password | links and many more Securely",
+                "Access Anywhere, Anytime ☁️",
+                "Top Privacy which You Can Trust 🛡️ ",
+              ],
+              autoStart: true,
+              loop: true,
+              delay: 75,
+              deleteSpeed: 30,
+            }}
+          />
+        </Text>
+      </Box>
+      <Box
+        width={{ base: "100%", md: "65%", lg: "60%" }}
+        minH={{ base: "20vh", md: "50vh", lg: "60vh" }}
         display="flex"
         justifyContent="center"
         alignItems="center"
         position="relative"
         overflow="hidden"
-        //   border={"2px solid lime"}
       >
         <video
           ref={videoRef}
