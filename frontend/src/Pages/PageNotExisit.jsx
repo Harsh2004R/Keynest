31f4d9a5cd46567b6eeb8d06c6347c5fefde5ab7
Harsh2004R <harshsharmaktm03@gmail.com>
2025-08-23 11:16:18 +0530

    UI Fixed --PageNotExist

--- frontend/src/Pages/PageNotExisit.jsx
+++ frontend/src/Pages/PageNotExisit.jsx
@@ -1,28 +1,22 @@
-import { Box, Heading, Text, Button } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { Heading, Text, Button, Center } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export default function PageNotExisit() {
-    return (
-        <Box textAlign="center" py={10} px={6}>
-            <Heading
-                as="h2"
-                size="2xl"
-                backgroundClip="text"
-                color="teal.400"
-            >
-                404
-            </Heading>
-            <Text fontSize="18px" mt={3} mb={2}>
-                Page Not Found
-            </Text>
-            <Text color={'gray.500'} mb={6}>
-                The page you&apos;re looking for does not seem to exist
-            </Text>
+  return (
+    <Center flexDirection={"column"} textAlign="center" w="100%" h="100vh" py={10} px={6}>
+      <Heading as="h2" size="2xl" backgroundClip="text" color="teal.400">
+        404
+      </Heading>
+      <Text fontSize="18px" mt={3} mb={2}>
+        Page Not Found
+      </Text>
+      <Text color={"gray.500"} mb={6}>
+        The page you&apos;re looking for does not seem to exist
+      </Text>
 
-            <Button
-            >
-                <Link to="/"> Go to Home</Link>
-            </Button>
-        </Box>
-    )
+      <Button>
+        <Link to="/"> Go to Home</Link>
+      </Button>
+    </Center>
+  );
 }
