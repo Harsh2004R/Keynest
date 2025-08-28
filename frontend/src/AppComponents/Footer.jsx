9b9846eaa917d80948743adeb0e88b2643001439
Harsh2004R <harshsharmaktm03@gmail.com>
2025-08-28 10:46:33 +0530

    Added --Footer.jsx Template

---
+++ frontend/src/AppComponents/Footer.jsx
@@ -0,0 +1,12 @@
+import { Box,Text } from '@chakra-ui/react'
+import React from 'react'
+
+function Footer() {
+  return (
+    <Box bg="#000">
+      Footer...
+    </Box>
+  )
+}
+
+export default Footer
